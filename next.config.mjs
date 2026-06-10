1429d00118b20fd6e9553756597335e351116531
kgolec93 <kamil.golec@protonmail.com>
2026-06-10 11:02:09 +0200

    Base path added in nextJS config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio-2025',
+  assetPrefix: '/portfolio-2025',
   images: {
     unoptimized: true
   }
